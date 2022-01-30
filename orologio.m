f4dfa0480998635ff05f64af39f7d72da6e4338c
Gerardo Bochicchio <github@gbochicchio.it>
2022-01-30 11:41:02 +0100

    Update orologio.m

--- orologio.m
+++ orologio.m
@@ -1,29 +1,15 @@
 function [] = orologio(oraInizio, minutiInizio, oraFine, minutiFine)
-    for i=1:inf
+    for i=1:inf % Warning: Too many FOR loop iterations. 
         clc;
         A=fix(clock);
         disp('ORA ATTUALE')
         stampaOra = strcat(int2str(A(4)),':',int2str(A(5)),':',int2str(A(6)));
         disp(stampaOra);
         disp('TEMPO TRASCORSO');
-        if(A(5)-minutiInizio<0)
-            stampaOra = strcat(int2str(A(4)-oraInizio),':',int2str(A(5)),':',int2str(A(6)));
-        else
-            stampaOra = strcat(int2str(A(4)-oraInizio),':',int2str(A(5)),':',int2str(A(6)));
-        end
+        stampaOra = strcat(int2str(A(4)-oraInizio),':',int2str(A(5)-minutiInizio),':',int2str(A(6)));
         disp(stampaOra);
         disp('TEMPO RIMANENTE');
-        if((oraFine-A(4))<=0)
-            if(minutiFine - A(5)<0)
-                stampaOra = strcat(int2str(oraFine-A(4)),':',int2str(-A(5)),':',int2str(-A(6)));
-            end
-        else
-            if(minutiFine - A(5)<0)
-                stampaOra = strcat(int2str(oraFine-A(4)-1),':',int2str(60-minutiFine-A(5)),':',int2str(60-A(6)));
-            else
-                stampaOra = strcat(int2str(oraFine-A(4)),':',int2str(60-minutiFine-A(5)),':',int2str(60-A(6)));
-            end
-        end
+        stampaOra = strcat(int2str(oraFine-A(4)-1),':',int2str(60 - A(5)+minutiFine),':',int2str(60-A(6)));
         disp(stampaOra);
         pause(1);
     end
